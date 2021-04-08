85b216b3c87af64ecde015ea5c101f3d1fdea7ff
TonySilv7 <tony.deve7oper@gmail.com>
2021-04-08 15:30:20 -0300

    Referencing user posts

--- src/main/java/com/tony/workshopmongo/config/Instantiation.java
+++ src/main/java/com/tony/workshopmongo/config/Instantiation.java
@@ -30,10 +30,15 @@ public class Instantiation implements CommandLineRunner {
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
 
         Post post1 = new Post(null, LocalDateTime.now(), "Partiu viagem", "Vou viajar para SP, abcs!!!", new AuthorDTO(maria));
         Post post2 = new Post(null, LocalDateTime.now(), "Bom dia!", "Acordei feliz hoje!", new AuthorDTO(maria));
+
         postRepository.saveAll(Arrays.asList(post1, post2));
+
+        maria.getPosts().addAll(Arrays.asList(post1, post2));
+        userRepository.save(maria);
     }
 }
