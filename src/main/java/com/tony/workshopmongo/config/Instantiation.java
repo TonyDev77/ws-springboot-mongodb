835f67ff57853884d0444521dd21a5b1a0ab1e34
TonySilv7 <tony.deve7oper@gmail.com>
2021-04-08 12:47:09 -0300

    Projection of author data with DTO

--- src/main/java/com/tony/workshopmongo/config/Instantiation.java
+++ src/main/java/com/tony/workshopmongo/config/Instantiation.java
@@ -2,6 +2,7 @@ package com.tony.workshopmongo.config;
 
 import com.tony.workshopmongo.domain.Post;
 import com.tony.workshopmongo.domain.User;
+import com.tony.workshopmongo.dto.AuthorDTO;
 import com.tony.workshopmongo.repository.PostRepository;
 import com.tony.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,11 +30,10 @@ public class Instantiation implements CommandLineRunner {
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
-
-        Post post1 = new Post(null, LocalDateTime.now(), "Partiu viagem", "Vou viajar para SP, abcs!!!", maria);
-        Post post2 = new Post(null, LocalDateTime.now(), "Bom dia!", "Acordei feliz hoje!", maria);
-
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+        Post post1 = new Post(null, LocalDateTime.now(), "Partiu viagem", "Vou viajar para SP, abcs!!!", new AuthorDTO(maria));
+        Post post2 = new Post(null, LocalDateTime.now(), "Bom dia!", "Acordei feliz hoje!", new AuthorDTO(maria));
         postRepository.saveAll(Arrays.asList(post1, post2));
     }
 }
